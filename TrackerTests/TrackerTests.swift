e40b1ac8331b13f4051440dfdddef453b33008bd
Arthur <Fokusnik90999@gmail.com>
2024-10-04 23:00:06 +0500

    re-added the tests

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -2,7 +2,7 @@
 //  TrackerTests.swift
 //  TrackerTests
 //
-//  Created by Артур  Арсланов on 01.10.2024.
+//  Created by Артур  Арсланов on 04.10.2024.
 //
 
 import XCTest
@@ -10,17 +10,16 @@ import SnapshotTesting
 @testable import Tracker
 
 final class TrackerTests: XCTestCase {
-    
+
     func testMainScreen() throws {
         let mainViewController = TrackersViewController()
-        
+
         mainViewController.overrideUserInterfaceStyle = .light
-        
+
         mainViewController.view.frame = UIScreen.main.bounds
-        
+
         mainViewController.viewDidLoad()
-        
+
         assertSnapshot(of: mainViewController, as: .image)
     }
 }
-
